0794255eca71b09b43679bc36c1df374c0fa98fe
javier <javier.san.mail@gmail.com>
2020-07-01 16:27:32 +0200

    added modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Modal, ModalHeader, ModalBody, Table } from "reactstrap";
+import "./Modal.css";
+
+const ModalComponent = (props) => {
+  const {
+    name,
+    manufacturer,
+    description,
+    color,
+    price,
+    imageFileName,
+    screen,
+    processor,
+    ram,
+    isVisible,
+    setVisible,
+  } = props;
+  const toggle = () => setVisible();
+
+  return (
+    <Modal isOpen={isVisible} toggle={toggle} size="lg">
+      <ModalHeader toggle={toggle} />
+      <ModalBody>
+        <div className="modal-body">
+          <div className="modal-img">
+            <img
+              src={process.env.PUBLIC_URL + `images/${imageFileName}`}
+              alt="img"
+            />
+          </div>
+          <div className="modal-description">
+            <h5>{name}</h5>
+            <p>{manufacturer}</p>
+            <p>{description}</p>
+            <p className="modal-price">{price} €</p>
+
+            <Table bordered>
+              <tbody>
+                <tr>
+                  <td>Color</td>
+                  <td>{color}</td>
+                </tr>
+                <tr>
+                  <td>screen</td>
+                  <td>{screen}</td>
+                </tr>
+                <tr>
+                  <td>processor</td>
+                  <td>{processor}</td>
+                </tr>
+                <tr>
+                  <td>ram</td>
+                  <td>{ram}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </ModalBody>
+    </Modal>
+  );
+};
+
+export default ModalComponent;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Modal from "./Modal";
 import "./Product.css";
 
 const Product = (props) => {
@@ -15,15 +16,56 @@ const Product = (props) => {
     ram,
   } = props;
 
+  const [visible, setVisible] = useState(false);
+  const [imageLoading, setImageLoading] = useState(true);
+
+  const toggleModal = (e) => {
+    if (e.target.className !== "avoid-modal") setVisible(true);
+  };
+
   return (
-    <div className="product-container">
+    <div
+      className="product-container"
+      data-test="productComponent"
+      onClick={(e) => toggleModal(e)}
+    >
+      {!imageLoading ? null : (
+        <div
+          data-test="product-div-loading"
+          style={{
+            background: "rgb(250, 250, 250)",
+            height: "200px",
+            width: "90%",
+            margin: "auto",
+            marginTop: "10px",
+          }}
+        />
+      )}
       <img
+        style={!imageLoading ? {} : { display: "none" }}
+        data-test="product-image"
         className="product-image"
         src={process.env.PUBLIC_URL + `images/${imageFileName}`}
         alt="img"
+        onLoad={() => setImageLoading(false)}
       />
       <p data-test="product-name">{name}</p>
       <p data-test="product-price">{price} €</p>
+      <Modal
+        key={id}
+        id={id}
+        name={name}
+        manufacturer={manufacturer}
+        description={description}
+        color={color}
+        price={price}
+        imageFileName={imageFileName}
+        screen={screen}
+        processor={processor}
+        ram={ram}
+        isVisible={visible}
+        setVisible={() => setVisible(!visible)}
+      />
     </div>
   );
 };

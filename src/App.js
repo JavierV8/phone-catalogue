5f62ef683659a0f68309a5ee74e9d73b001ecd5b
javier <javier.san.mail@gmail.com>
2020-07-01 16:04:43 +0200

    added actions and reducer for products

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getProducts } from "./store/actions/Products";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const products = useSelector((state) => state.products.products);
+  const isLoading = useSelector((state) => state.products.isLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
+
+  console.log(products);
+  console.log(isLoading);
+
+  return <div className="App-container"></div>;
 }
 
 export default App;

50e434ed849f6609013ee1c8d722110a0ae7b7fa
javier <javier.san.mail@gmail.com>
2020-07-02 10:15:33 +0200

    fixed some errors

--- server.js
+++ server.js
@@ -2,14 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const phones = require("./phones.json");
+const cors = require("cors");
 
-// statics assets if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(cors());
 
 app.get("/phones", (req, res) => {
   res.header("Content-Type", "application/json");
